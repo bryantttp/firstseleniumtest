43ebb733b5db629d0e65c422ce40cbf9ea2f0376
bryant pang <bryant.pang@fdmgroup.com>
2024-09-09 17:04:02 +0800

    Add methods and attributes to ProductsPage

--- src/main/java/com/saucedemo/pages/ProductsPage.java
+++ src/main/java/com/saucedemo/pages/ProductsPage.java
@@ -1,5 +1,11 @@
 package com.saucedemo.pages;
 
-public class ProductsPage extends BasePage{
+import org.openqa.selenium.By;
 
+public class ProductsPage extends BasePage{
+    private By productsHeader = By.xpath("//span[text()='Products']");
+
+    public boolean isProductsHeaderDisplayed(){
+        return find(productsHeader).isDisplayed();
+    }
 }
